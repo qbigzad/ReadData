56114641bef50454bdc22c3305d2458105279a6e
Mohammad Qasim Bigzad <qbigzad@gmail.com>
2022-07-24 22:31:59 +0430

    Read Data Application

---
+++ ExtractInfo.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReadData
+{
+    public class ExtractInfo
+    {
+        public string FindString { get; set; }
+
+        public string FindStringKeyWord { get; set; }
+        public string FilePath { get; set; }
+        public string ArrayKeyWord { get; set; }
+        public int startLineSearch { get; set; }
+
+        public List<string> getData()
+        {
+            List<string> ExtractedInfo = new List<string>();
+            string lines = "";
+            LinkedList<string> ls = new LinkedList<string>();
+
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                while ((lines = sr.ReadLine()) != null)
+                {
+                    ls.AddLast(lines);
+                }
+            }
+            int i = 0;
+            int linePointer = startLineSearch;
+            int lastLinePointer = 0;
+            foreach (var item in ls)
+            {
+                int lineNumber = 0;
+                
+                if (FindStringKeyWord!=null && item.Contains(FindStringKeyWord))
+                {
+                    FindString = item;
+                }
+                if (item.Contains("ARRAY") && item.Contains(ArrayKeyWord))
+                {
+
+                    string checkS = Regex.Replace(item, @"[^\d]", "");
+                    if (checkS.Length == 3)
+                    {
+                        lineNumber = Convert.ToInt32(checkS.Substring(2));
+                    }
+                    else if (checkS.Length == 4)
+                    {
+                        lineNumber = Convert.ToInt32(checkS.Substring(3));
+                    }else if (checkS.Length == 2)
+                    {
+                        lineNumber = Convert.ToInt32(checkS.Substring(1));
+                    }
+                    else
+                    {
+                        lineNumber = Convert.ToInt32(checkS);
+                    }
+
+
+                    linePointer = i;
+                    lastLinePointer = linePointer + lineNumber;
+
+                }
+
+                if (i > linePointer && i <= lastLinePointer)
+                {
+                    string textFormat = Regex.Replace(item, @"[^0-9a-zA-Z:,+-.]+", "");
+                    textFormat = Regex.Replace(textFormat, @"[a-z]|[A-Z]", "");
+                    if (textFormat.Length == 1)
+                    {
+                        textFormat = Regex.Replace(textFormat, @"[0-9]", "");
+                    }
+
+                    int countLength = textFormat.Length;
+                    string formatTextSub = "";
+                    if (textFormat != "")
+                    {
+                        formatTextSub = textFormat.Substring(1, countLength - 1);
+                    }
+
+                    try
+                    {
+                        ExtractedInfo.Add(formatTextSub);
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine(e.ToString());
+                    }
+
+
+
+                }
+
+
+                i++;
+
+
+            }
+
+            return ExtractedInfo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ReadData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractInfo refpos1 = new ExtractInfo();
+            Console.WriteLine("----------------------Search System.va------------------------");
+            //Search the array by its keyword inside the file
+            refpos1.ArrayKeyWord = "REFPOS1";
+
+            //find the string keyword inside the file
+            refpos1.FindStringKeyWord = "HOSTNAME";
+            refpos1.startLineSearch = 34751;
+            string FilePath = "C://files//2022-07-22//TD020R01B06//md//system.va";
+            refpos1.FilePath = FilePath;
+            List<string> formatArray = new List<string>();
+            formatArray = refpos1.getData();
+
+            //Show the found string keyword.
+            Console.WriteLine(refpos1.FindString);
+
+            //show the list of arrays found by the array keyword
+            foreach (var num in formatArray)
+            {
+                Console.WriteLine(num.ToString());
+            }
+            Console.WriteLine("----------------------Search Sysmat.va------------------------");
+            ExtractInfo sysmast = new ExtractInfo();
+            sysmast.ArrayKeyWord = "DMR_GRP[1].$MASTER_COUN";
+            string FilePath1 = "C://files//2022-07-22//TD020R01B06//md//sysmast.va";
+            sysmast.startLineSearch = 23;
+            sysmast.FilePath = FilePath1;
+            List<string> sysmatList = new List<string>();
+            sysmatList = sysmast.getData();
+            //show the list of arrays found by the array keyword
+            foreach (var num in sysmatList)
+            {
+                Console.WriteLine(num.ToString());
+            }
+            Console.WriteLine("----------------------Search Numreg.va------------------------");
+            ExtractInfo numreg = new ExtractInfo();
+            numreg.ArrayKeyWord = "$NUMREG  Storage";
+            numreg.FindStringKeyWord = "MAXREGNUM";
+            string FileString = "C://files//2022-07-22//TD020R01B06//md//numreg.va";
+            numreg.startLineSearch = 2;
+            numreg.FilePath = FileString;
+            List<string> numregList = new List<string>();
+            numregList = numreg.getData();
+            Console.WriteLine(numreg.FindString);
+            //show the list of arrays found by the array keyword
+            foreach (var num in sysmatList)
+            {
+                Console.WriteLine(num.ToString());
+            }
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
